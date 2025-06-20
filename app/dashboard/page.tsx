609b66f89c867fcb57521986c664349b3958151b
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2025-06-20 11:14:08 -0700

    fix: another try for the test barcode with the new package

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import {
     Card
 } from "@/components/ui/card"
 
-import Barcode from 'react-barcode';
+import { useBarcode } from 'next-barcode';
 
 import { CardSim } from "lucide-react"
 
@@ -21,13 +21,19 @@ import Image from 'next/image'
 
 export default function Page() {
     const device:string = "WORKSTATION M507" //Change to let and proper updating when the time comes, use "use-state"
+    const { deviceBarCode } = useBarcode({
+        value: 'knf98976-th34yk',
+        options: {
+        background: '#f1f5f9',
+        }
+    });
 
     return (
         <>
         <div className="w-dvw h-dvh p-8">
             <div className="w-full p-2 pb-0 mt-8 mb-12">
                 <Card className="bg-slate-100">
-                    <Barcode value="mvz1210dg89yk"/>
+                    <svg ref={deviceBarCode} className="w-full h-12"/>
                 </Card>
             </div>
             <div className="w-full aspect-8560/5398 relative">
