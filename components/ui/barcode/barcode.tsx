cc6c4553610419d55a14a292e60c2646c1121f4e
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2025-06-20 11:59:10 -0700

    fix: tried a new format for the barcode to see if it works

--- components/ui/barcode/barcode.tsx
+++ components/ui/barcode/barcode.tsx
@@ -2,18 +2,13 @@
 import React from 'react';
 import { useBarcode } from 'next-barcode';
 
-export function BarCode({
-  code
-}:{
-    code:string
-}) {
-    const { deviceBarCode } = useBarcode({
-        value: code,
-        options: {
-        background: '#f1f5f9',
-        }
-    });
+export function BarCode({ code }: { code: string }) {
+  const { inputRef } = useBarcode({
+    value: code,
+    options: {
+      background: '#f1f5f9',
+    },
+  });
 
-  return <img ref={deviceBarCode} className="w-full h-12"/>
-  
+  return <img ref={inputRef} alt="Barcode" className="w-full h-12" />;
 }
